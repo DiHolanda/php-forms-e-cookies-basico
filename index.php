329eff7039b66f694623fc2bb4d9a93b9de300c0
DiHolanda <diegopessoal@protonmail.com>
2022-04-04 09:39:59 -0300

    Introdução - construção da página index.php com o formulário

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form action="index.php" method="">
+		
+		<input type="text" name="nome">
+		<input type="submit" value="Enviar">
+
+	</form>
+</body>
+</html>
