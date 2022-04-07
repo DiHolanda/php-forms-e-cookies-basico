303631ec5e21c02ff68a5c97b50573affa260db3
DiHolanda <diegopessoal@protonmail.com>
2022-04-07 10:29:33 -0300

    Filtragem de espaços vazios, para um email, por exemplo

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-	
-	// ?? é null coalescing operator, ou seja, define null se o valor estiver vazio
-	$idade = (int)$_POST['idade'] ?? null;
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		$email = $_POST['email'] ?? null;
+		$email = trim($email);
+		var_dump($email);
+	}
 
-	if(is_null($idade) || $idade == 0){
-		die("Você deveria informar a sua idade");
-	}
-	
-	if($idade < 18){
-		die("Site impróprio para menores de idade");
-	}
-	else{
-		echo "Pode acessar, você tem " . $idade . " anos";
-	}
-	
-}
-
-?>
+ ?>
 
 <html lang="pt_BR">
 <head>
@@ -29,11 +17,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 	<form action="index.php" method="post">
 		
-		<input type="text" name="idade" placeholder="idade">
+		<input type="email" name="email">
 		<input type="submit" value="Enviar">
 
 	</form>
 
-	
 </body>
 </html>
