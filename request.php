9836de029ae0b471d2de99d7b290ac8484f7c3a2
DiHolanda <diegopessoal@protonmail.com>
2022-04-08 10:10:15 -0300

    Demonstração da super variável $_REQUEST

--- request.php
+++ request.php
@@ -1,11 +1,10 @@
 <?php 
-
-	if ($_SERVER["REQUEST_METHOD"] == "POST"){
-		
-		
-		
+	if(!empty($_REQUEST['nome'])){
+		$nome = $_REQUEST['nome'];
 	}
-
+	else{
+		$nome = "ninguém";
+	}
  ?>
 
 <html lang="pt_BR">
@@ -15,12 +14,19 @@
 	<title>PHP Formulário</title>
 </head>
 <body>
-	<form action="index.php" method="post">
+	<form action="request.php" method="post">
 		
 		<input type="text" name="nome">
 		<input type="text" name="idade">
 		
 		<input type="submit" value="Enviar">
+
+		<hr>
+
+		<a href="request.php?nome=Anônimo"> Sou anônimo </a>
+		<p>  Visitante se identificou como <?php //echo (!empty($_GET["nome"])) ? "O " . $_GET['nome'] : "Ninguém " ; 
+			echo $nome;
+	?>  </p>
 
 	</form>
 
