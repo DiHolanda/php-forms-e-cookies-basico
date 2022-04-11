2bf6c832c844cdd593ab81d5a5239d2cd6c5fd6b
DiHolanda <diegopessoal@protonmail.com>
2022-04-11 11:28:35 -0300

    Salvando informações de sessão em uma pasta

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php 
 
+	// define onde as informações serão salvas na sessão
+	session_save_path(__DIR__.'\sessions');
+
 	// começa uma sessão
 	session_start();
+
+
 
 	// define o cookie com o próprio nome, a expurar em um dia (3600 segundos equivale a uma hora)
 	//setcookie('meu-nome', "Di Holanda", time() + ((3600 * 24) * 365));
 
 	// o cookie chega nem a aparecer se estiver fora da validade, como setando sua expiração em um segundo a menos
 	// setcookie('meu-nome', "Di Holanda", time() - 1);
+
+	$_SESSION['meu-nome'] = 'Di Holanda';
 
  ?>
--- outra-pagina/index.php
+++ outra-pagina/index.php
@@ -1,5 +1,7 @@
 <?php 
 
-	echo $_COOKIE['meu-nome'];
+	// mostra o local onde armazena a informação salva em sessão
+	// __DIR__ pega o caminho do arquivo atual =
+	var_dump(session_save_path(__DIR__.'\..\sessions'));
 
  ?>
