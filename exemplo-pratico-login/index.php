b67e3b1bda0136b253e7cdff4c42b0f4163807e9
DiHolanda <diegopessoal@protonmail.com>
2022-04-11 15:23:14 -0300

    Verificar se o usuário está logado, senão transfere para a página de login

--- exemplo-pratico-login/index.php
+++ exemplo-pratico-login/index.php
@@ -1,5 +1,13 @@
 <?php 
 
+session_start();
 
+$user = $_SESSION['user'] ?? null;
+
+// se o usuário não estiver autenticado, transfere para a página de login e interrompe a execução da página atual
+if(!$user){
+	header('location: ./login.php');
+	exit();
+}
 
  ?>
