bcee3a192c58da07a31473252591013ea2ee0788
DiHolanda <diegopessoal@protonmail.com>
2022-04-11 11:10:42 -0300

    Tirando a validade do cookie com menos um segundo para expiração

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 
 	// define o cookie com o próprio nome, a expurar em um dia (3600 segundos equivale a uma hora)
-	setcookie('meu-nome', "Di Holanda", time() + ((3600 * 24) * 365));
+	//setcookie('meu-nome', "Di Holanda", time() + ((3600 * 24) * 365));
+
+	// o cookie chega nem a aparecer se estiver fora da validade, como setando sua expiração em um segundo a menos
+	setcookie('meu-nome', "Di Holanda", time() - 1);
 
  ?>
