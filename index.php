a6d26586111ea816096f2d3b51cd9c660d54aa1b
DiHolanda <diegopessoal@protonmail.com>
2022-04-11 11:01:52 -0300

    Cookie criado com o próprio nome, com validade de um ano

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 
-	setcookie('');
+	// define o cookie com o próprio nome, a expurar em um dia (3600 segundos equivale a uma hora)
+	setcookie('meu-nome', "Di Holanda", time() + ((3600 * 24) * 30) * 12);
 
  ?>
