7f62cf253379439d8fc1c958a608d5426790d146
DiHolanda <diegopessoal@protonmail.com>
2022-04-12 10:04:22 -0300

    Definição e comentários sobre segurança para cookie de sessão

--- exemplo-pratico-login/session.php
+++ exemplo-pratico-login/session.php
@@ -1,5 +1,16 @@
 <?php 
 
+/*define o cookie de sessão 
+	* com validade de uma hora após fechar o navegador
+	* o cookie será usado apenas nos arquivos sob a pasta exemplo-pratico-login
+	* é possível tornar o cookie válido apenas em determinado domínio (no caso deste exemplo prático onde é executado em ambiente local, fica com o valor padrão null, ou seja, válido a partir do domínio criado)
+	** porém é interessante considerar se este cookie for funcionar em todo o domínio de um site real, ou funcionar apenas em certo subdomínio
+	* a quarta configuração é usar cookie apenas em ambiente seguro, sob servidor https
+	** por padrão é false, então se o site tiver conexão HTTPS, melhor deixar true
+	* e por último é para determinar que o cookie seja usado apenas por protocolo HTTP, impossibilitando um script de javascript, por exemplo
+*/
+session_set_cookie_params(60*60, '/exemplo-pratico-login', null, false, true);
+
 session_start();
 
  ?>
