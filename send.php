651b0dad85ca4ce74cbe8e9a70606afa5db38ce2
DiHolanda <diegopessoal@protonmail.com>
2022-04-18 10:12:11 -0300

    Recebimento das entradas do formulário em send.php

---
+++ send.php
@@ -0,0 +1,8 @@
+<?php 
+
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$mensagem = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+ ?>
