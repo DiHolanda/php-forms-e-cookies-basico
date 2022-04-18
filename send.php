18971f1d015eb9e424c17a067bfe2d69767ae5a6
DiHolanda <diegopessoal@protonmail.com>
2022-04-18 10:14:40 -0300

    Testando a entrada de dados com var_dump - e alinhando os inputs com br

--- send.php
+++ send.php
@@ -4,5 +4,6 @@ $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $mensagem = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 
+var_dump($nome, $email, $mensagem);
 
  ?>
