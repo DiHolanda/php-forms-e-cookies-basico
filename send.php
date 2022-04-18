d4fa091c3a3017f6a728f31074e9c373bc219b15
DiHolanda <diegopessoal@protonmail.com>
2022-04-18 10:07:03 -0300

    Criação de novo formulário para aprofundar na mecânica dos formulários

---
+++ send.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<!-- atalho emmet form>input*4 já cria as tagas form e quatro inputs -->
+	<form action="send.php">
+		<input type="text" name="nome" placeholder="Nome">
+		<input type="email" name="email" placeholder="Email">
+		<textarea name="mensagem" placeholder="Mensagem"></textarea>
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
