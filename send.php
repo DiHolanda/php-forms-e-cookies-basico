25b787b572c866d1de6e8eeac6e1d4963e44eb9e
DiHolanda <diegopessoal@protonmail.com>
2022-04-19 13:51:27 -0300

    Recebendo os valores transmitidos através de json

--- send.php
+++ send.php
@@ -1,9 +1,19 @@
 <?php 
 
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+// exemplo de como pegar dados passados por formulário
+// até aqui, nenhuma novidade
+/*$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_SPECIAL_CHARS);
 
-var_dump($nome, $email, $mensagem);
+var_dump($nome, $email, $mensagem);*/
+
+// pega o conteúdo transferido em formulário via json e o salva na variável como string
+$json = file_get_contents("php://input");
+
+// a seguir converte o que foi salvo em string para o objeto de json
+$json = json_decode($json);
+
+var_dump($json);
 
  ?>
