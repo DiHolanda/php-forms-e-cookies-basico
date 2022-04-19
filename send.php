24a762e57d7206c2bd2fe63b208d97e07d417124
DiHolanda <diegopessoal@protonmail.com>
2022-04-19 14:05:46 -0300

    Garantindo o valor passado em variáveis, seja por meio de form-data ou json

--- send.php
+++ send.php
@@ -2,11 +2,11 @@
 
 // exemplo de como pegar dados passados por formulário
 // até aqui, nenhuma novidade
-/*$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_SPECIAL_CHARS);
 
-var_dump($nome, $email, $mensagem);*/
+var_dump("--FORM-DATA--", $nome, $email, $mensagem);
 
 // pega o conteúdo transferido em formulário via json e o salva na variável como string
 $json = file_get_contents("php://input");
@@ -17,6 +17,22 @@ $json = json_decode($json);
 // da forma a seguir, converte em array em vez de objeto
 // $json = json_decode($json, true);
 
-var_dump($json);
+var_dump("--JSON--", $json);
+
+
+if(is_null($nome)){
+    $nome = $json->nome ?? null;
+}
+
+if(is_null($email)){
+    $email = $json->email ?? null;
+}
+
+if(is_null($mensagem)){
+    $mensagem = $json->mensagem ?? null;
+}
+
+// garante que os valores sejam passados para as variáveis, seja através de form-data ou json
+var_dump("--FINAL FANTASY--", $nome, $email, $mensagem);
 
  ?>
